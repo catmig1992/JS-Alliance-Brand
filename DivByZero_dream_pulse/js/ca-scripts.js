ab553c414d9b365056e6dea54a8e9e6150817243
Catherine Miguel <catmig1992@gmail.com>
2022-10-02 13:10:55 -0700

    first portion

--- DivByZero_dream_pulse/js/ca-scripts.js
+++ DivByZero_dream_pulse/js/ca-scripts.js
@@ -2,39 +2,29 @@ var customerName = "Alliance  ";
 
 // <h1 id="tm-header" class="tm-brand text-uppercase"></h1> <!--todo: Add Customer Name-->
 function addCustomerName() {
+  let title = document.getElementById("title");
+  title.textContent = customerName;
 
-    let title = document.getElementById('title')
-    title.textContent = customerName;
-    
-    // <h1 id="tm-header" class="tm-brand text-uppercase"></h1> <!--todo: Add Customer Name-->
-
-    // <h2 id="introheader" class="tm-section-title mb-5 text-uppercase tm-color-primary"></h2> <!--todo: Add Customer Name-->
-
-};
+  // <h1 id="tm-header" class="tm-brand text-uppercase"></h1> <!--todo: Add Customer Name-->
+  let header = document.getElementById("tm-header");
+  header.textContent = customerName;
+  // <h2 id="introheader" class="tm-section-title mb-5 text-uppercase tm-color-primary"></h2> <!--todo: Add Customer Name-->
+  let introheader = document.getElementById("introheader");
+  introheader.textContent = customerName;
+}
 
 function toggleColors() {
-    // <!-- todo: Change all classes named 'tm-white-rect' to 'tm-blue-rect' -->
-
-};
+  // <!-- todo: Change all classes named 'tm-white-rect' to 'tm-blue-rect' -->
+}
 
 function hideElement() {
-    //<ul class="nav flex-row tm-social-links">  <!--todo: Hide social links-->
+  //<ul class="nav flex-row tm-social-links">  <!--todo: Hide social links-->
+}
 
-};
-
- function addText() {
-
-    //<p id="intropara1" class="tm-color-gray">  <!--todo: Add customer company introduction text about their founding year-->
-
-    //<p id="intropara2" class="mb-0 tm-color-gray"> <!--todo: Add customer company introduction text about what they lead in-->
-
-     //<p id="aboutpara1" id="a" class="mb-5"> <!--todo: Add customer company about text-->
-
-    //<p id="aboutpara2"> <!--todo: Add customer company motto text-->
-
-    // <p id="addresspara" class="mb-3"> <!--todo: Add customer company phone number text-->
-
-}; 
-
-
-
+function addText() {
+  //<p id="intropara1" class="tm-color-gray">  <!--todo: Add customer company introduction text about their founding year-->
+  //<p id="intropara2" class="mb-0 tm-color-gray"> <!--todo: Add customer company introduction text about what they lead in-->
+  //<p id="aboutpara1" id="a" class="mb-5"> <!--todo: Add customer company about text-->
+  //<p id="aboutpara2"> <!--todo: Add customer company motto text-->
+  // <p id="addresspara" class="mb-3"> <!--todo: Add customer company phone number text-->
+}
